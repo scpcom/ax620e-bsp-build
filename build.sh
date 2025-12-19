3013deec51da8a5ad6c2c6083d643a815fe4f336
scpcom <scpcom@gmx.de>
2025-12-19 20:00:22 +0100

    build.sh: add option --board

--- build.sh
+++ build.sh
@@ -1,6 +1,25 @@
 #!/bin/sh -e
 . ./scripts/envsetup_pack.sh
 
+AX_BOARD_LINK=$BOARD_DTS
+
+while [ "$#" -gt 0 ]; do
+	case "$1" in
+	--board=*|--board-link=*)
+		export AX_BOARD_LINK=`echo $1 | cut -d '=' -f 2-`
+		shift
+		;;
+	*)
+		break
+		;;
+	esac
+done
+
+if [ "$AX_BOARD_LINK" != "$BOARD_DTS" ]; then
+  sed -i s/'^BOARD_DTS=.*'/'BOARD_DTS='$AX_BOARD_LINK/g  ./scripts/envsetup_pack.sh
+  . ./scripts/envsetup_pack.sh
+fi
+
 ./scripts/get-toolchain.sh
 
 ./scripts/build-u-boot.sh
