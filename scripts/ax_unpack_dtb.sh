cd92624755bd0183c2d6ff6b74166d661da28aef
scpcom <scpcom@gmx.de>
2025-12-19 03:09:17 +0100

    scripts: add ax_unpack_dtb.sh

---
+++ scripts/ax_unpack_dtb.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+#GERNERAL_BIN=`pwd`/general_bin_ax630c
+GERNERAL_BIN=`pwd`/axerabin/tools/bin
+PACKED_BIN=$1
+
+[ "X$PACKED_BIN" != "X" ] || exit 1
+
+./scripts/ax_unpack_bin.sh $PACKED_BIN
+dtc -I dtb -o dts -o ${PACKED_BIN}.dts ${PACKED_BIN}.unpacked
+
+echo OK
