76081573a210ccced2f1788a3a251aadb9e1efe0
scpcom <scpcom@gmx.de>
2025-12-29 16:25:42 +0100

    scripts: ax_sign_bin.sh: allow sign without padding

--- scripts/ax_sign_bin.sh
+++ scripts/ax_sign_bin.sh
@@ -22,7 +22,11 @@ python3 ${GERNERAL_BIN}/imgsign/sec_boot_AX620E_sign.py -i ${PACK_OUTPUT_DIR}/${
 	 -pub ${PACK_OUTPUT_DIR}/public.pem \
 	 -prv ${PACK_OUTPUT_DIR}/private.pem -cap 0x54FAFE -key_bit 2048 -o ${PACK_INSTALL_DIR}/${PACKED_BIN}
 
-dd if=/dev/zero bs=${PACKED_MAX_SIZE} count=1 | tr '\000' '\377' > ${PACK_INSTALL_DIR}/${PACKED_BIN}.tmp
-dd if=${PACK_INSTALL_DIR}/${PACKED_BIN} of=${PACK_INSTALL_DIR}/${PACKED_BIN}.tmp bs=${PACKED_MAX_SIZE} count=1 conv=notrunc seek=0
+if [ "${PACKED_MAX_SIZE}" = "-" ]; then
+  dd if=${PACK_INSTALL_DIR}/${PACKED_BIN} of=${PACK_INSTALL_DIR}/${PACKED_BIN}.tmp conv=notrunc
+else
+  dd if=/dev/zero bs=${PACKED_MAX_SIZE} count=1 | tr '\000' '\377' > ${PACK_INSTALL_DIR}/${PACKED_BIN}.tmp
+  dd if=${PACK_INSTALL_DIR}/${PACKED_BIN} of=${PACK_INSTALL_DIR}/${PACKED_BIN}.tmp bs=${PACKED_MAX_SIZE} count=1 conv=notrunc seek=0
+fi
 
 echo OK
