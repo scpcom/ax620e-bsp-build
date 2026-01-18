bc3f5b380b98d5c3d0cb91f6afb4efb00a4d1667
scpcom <scpcom@gmx.de>
2026-01-18 19:42:17 +0100

    scripts: build-linux.sh: add option to clean up

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -5,6 +5,11 @@ KERNEL_OUTPUT_DIR=$PACK_OUTPUT_DIR/linux
 KERNEL_CFG=m5stack_AX630C_emmc_arm64_k419_defconfig
 [ "${BOARD_DTS}" != "nanokvm_pro_arm64_k419" ] || KERNEL_CFG=nanokvm_pro_emmc_arm64_k419_defconfig
 
+if [ "X$1" = "Xclean" ]; then
+  make -C linux ARCH=${KERNEL_ARCH} CROSS_COMPILE=$CROSS_COMPILE O=$KERNEL_OUTPUT_DIR clean
+  exit 0
+fi
+
 cd linux
 if [ -e $PACK_INSTALL_DIR/initramfs_rootfs.cpio ]; then
   sed -i 's|^CONFIG_INITRAMFS_SOURCE=.*|CONFIG_INITRAMFS_SOURCE="../../../install/'${BOARD_DTS}'/initramfs_rootfs.cpio"|g' arch/${KERNEL_ARCH}/configs/${KERNEL_CFG}
