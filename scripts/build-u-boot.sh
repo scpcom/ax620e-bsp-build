7d96de416e262fa777b12b696492424754ab53e9
scpcom <scpcom@gmx.de>
2026-01-26 19:20:04 +0100

    scripts: build-u-boot.sh: add maixcam2

--- scripts/build-u-boot.sh
+++ scripts/build-u-boot.sh
@@ -5,6 +5,7 @@ UBOOT_OUTPUT_DIR=$PACK_OUTPUT_DIR/u-boot
 
 BOARD_CFG=AX630C_m5stack_LLM_module_defconfig
 [ "${BOARD_DTS}" != "m5stack-ax630c-lite" ] || BOARD_CFG=AX630C_m5stack_LITE_defconfig
+[ "${BOARD_DTS}" != "maixcam2_arm64_k419" ] || BOARD_CFG=maixcam2_defconfig
 [ "${BOARD_DTS}" != "nanokvm_pro_arm64_k419" ] || BOARD_CFG=nanokvm_pro_defconfig
 
 make -C u-boot ARCH=${UBOOT_ARCH} CROSS_COMPILE=$CROSS_COMPILE O=$UBOOT_OUTPUT_DIR dtb-y=${BOARD_DTS}.dtb EXTRA_CFLAGS=-DUBOOT_IMG_HEADER_BASE=0x5C000000 DEVICE_TREE=${BOARD_DTS} ${BOARD_CFG}
