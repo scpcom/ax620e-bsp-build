c024086610bdad3a6beda04519669962c44a1e6c
scpcom <scpcom@gmx.de>
2026-07-12 15:38:39 +0200

    scripts: build-u-boot.sh: enable UBOOT_SPI2_SIPEED_LOGO for nanoagent

--- scripts/build-u-boot.sh
+++ scripts/build-u-boot.sh
@@ -10,7 +10,7 @@ BOARD_CFG=AX630C_m5stack_LLM_module_defconfig
 [ "${BOARD_DTS}" != "nanokvm_pro_arm64_k419" ] || BOARD_CFG=nanokvm_pro_defconfig
 
 cd u-boot
-[ "${BOARD_DTS}" = "nanokvm_pro_arm64_k419" ] || sed -i /'^UBOOT_SPI2_SIPEED_LOGO'/d Makefile
+[ "${BOARD_DTS}" = "nanokvm_pro_arm64_k419" -o "${BOARD_DTS}" = "AX620Q_emmc_arm32_k419_sipeed_nanoagent" ] || sed -i /'^UBOOT_SPI2_SIPEED_LOGO'/d Makefile
 cd ..
 
 make -C u-boot ARCH=${UBOOT_ARCH} CROSS_COMPILE=$CROSS_COMPILE O=$UBOOT_OUTPUT_DIR dtb-y=${BOARD_DTS}.dtb BOARD=${BOARD_FAMILY} EXTRA_CFLAGS=-DUBOOT_IMG_HEADER_BASE=0x5C000000 DEVICE_TREE=${BOARD_DTS} ${BOARD_CFG}
